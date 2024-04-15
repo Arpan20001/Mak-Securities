bb3e98adb7225b8042f7ae2a8a06f6414bf511e0
MAK securities <166061244+Arpan20001@users.noreply.github.com>
2024-04-16 00:24:09 +0530

    Add files via upload

---
+++ assets/php/login.php
@@ -0,0 +1,22 @@
+<?php
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+   $select_admin = $conn->prepare("SELECT * FROM `mak` WHERE name = ? AND password = ?");
+   $select_admin->execute([$username, $password]);
+   $row = $select_admin->fetch(PDO::FETCH_ASSOC);
+
+   if($select_admin->rowCount() > 0){
+      $_SESSION['admin_id'] = $row['id'];
+      header('location:dashboard.php');
+   }else{
+      $message[] = 'incorrect username or password!';
+   }
+
+}
+
+?>
